eeeb5e9470fa90ed07b13ac3b293343bec90fdda
Mike Khaw <mikekhawnow@gmail.com>
2025-03-18 10:08:22 -0700

    Save

---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const FindRide = () => {
+  return (
+    <View>
+      <Text>find-ride</Text>
+    </View>
+  );
+};
+
+export default FindRide;
